51e1c74e7ca1366a12acf0864ea9efc1306c4f2a
Dylan Smith <dylan_smartguy@hotmail.com>
2017-07-08 13:35:04 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index', as: 'home'
+
+  resources :posts
+
+  resources :conversations, only: [:index, :create, :destroy] do
+    resource :messages, only: [:show, :new, :create, :destroy]
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :conversations, only: [:index, :create] do
+        resources :messages, only: [:show, :new, :create, :destroy]
+      end
+    end
+  end
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+  resources :maps, only: :index
+
+  mount ActionCable.server => '/cable'
 end
